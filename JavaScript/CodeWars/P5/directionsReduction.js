3974a65a99912a3fff24389434f1b97f6fa0af49
yilia.hong <yilia.hong@ringcentral.com>
2019-06-10 10:56:40 +0800

    P5: update directionsReduction

--- JavaScript/CodeWars/P5/directionsReduction.js
+++ JavaScript/CodeWars/P5/directionsReduction.js
@@ -1,12 +1,33 @@
+/*
+Write a function dirReduc which will take an array of strings 
+and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
+The Haskell version takes a list of directions with data Direction = North | East | West | South. 
+The Clojure version returns nil when the path is reduced to nothing. 
+The Rust version takes a slice of enum Direction {NORTH, SOUTH, EAST, WEST}.
+*/
+
 const assert = require('assert');
 
 function dirReduc(arr){
-    if(arr.length === 1){
-        return arr[0];
+    let brr = [];
+    for (let a = 0; a < arr.length; a++){
+        brr[a] = arr[a];
     }
-    arr.map((a, i) => {
-        arr[i]
-    });
+    if(brr.length === 1){
+        return brr[0];
+    }
+    else {
+        for (let i = 0; i< arr.length; i++){
+            switch (arr.length > 1){
+                case arr.slice(i, i+2) = ["NORTH", "SOUTH"]: brr.shift(); brr.shift();break;
+                case arr.slice(i, i+2) = ["SOUTH", "NORTH"]: brr.shift(); brr.shift();break;
+                case arr.slice(i, i+2) = ["EAST", "WEST"]: brr.shift(); brr.shift();break;
+                case arr.slice(i, i+2) = ["WEST", "EAST"]: brr.shift(); brr.shift();break;
+                default: break;
+            };
+        };
+        return brr;
+    };
 }
 
 assert(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]) === ["WEST"])
